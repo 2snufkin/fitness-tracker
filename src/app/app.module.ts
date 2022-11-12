71095f25de8cb9471c4a51123b9533cae4d91f0d
Pini <pini.tzr@gmail.com>
2022-11-12 12:36:45 +0100

    adding flexLayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { TrainingComponent } from './training/training/training.component';
-import { PastComponent } from './training/past/past.component';
-import { CurrentComponent } from './training/current/current.component';
-import { NewComponent } from './training/new/new.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LoginComponent} from './auth/login/login.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {TrainingComponent} from './training/training/training.component';
+import {PastComponent} from './training/past/past.component';
+import {CurrentComponent} from './training/current/current.component';
+import {NewComponent} from './training/new/new.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 import {RoutingModule} from './app.routing'
 import {MaterialModule} from "./material.module";
 import {FormsModule} from "@angular/forms";
+import {FlexLayoutModule} from "@angular/flex-layout";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,9 +31,12 @@ import {FormsModule} from "@angular/forms";
     BrowserAnimationsModule,
     RoutingModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
