b4ad24477912abcbf17ca4a71e8b574575e6fd21
Pini <pini.tzr@gmail.com>
2022-11-19 23:44:55 +0100

    added training tabs

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,6 +9,7 @@ import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatListModule} from "@angular/material/list";
+import {MatTabsModule} from "@angular/material/tabs";
 
 @NgModule({
   imports: [MatFormFieldModule,
@@ -31,7 +32,8 @@ import {MatListModule} from "@angular/material/list";
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatTabsModule
   ]
 })
 
