5af167606bf4252ad55aa2f03d48e602dac82a27
Pini <pini.tzr@gmail.com>
2022-11-28 06:54:43 +0100

    training spinner is working but need to be stoped correctly

--- src/app/training-card/training-card.component.ts
+++ src/app/training-card/training-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-training-card',
@@ -11,8 +11,10 @@ export class TrainingCardComponent implements OnInit {
   @Input() desc: string | undefined;
   @Output() eventEmmiter: EventEmitter<boolean> = new EventEmitter();
   isClicked: boolean = false;
-  running= false;
+  is_running= false;
+  is_show= false;
   @Input() progress: number | undefined;
+  clock_progress = 0;
 
 
 
@@ -22,15 +24,21 @@ export class TrainingCardComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  emitToParent($event: MouseEvent) {
-    console.log($event.button);
-    this.eventEmmiter.emit(!this.isClicked)
-
-  }
 
   toogle_spinner() {
-    this.running = !this.running;
-    console.log(this.running)
-    this.eventEmmiter.emit(this.running)
+    if (!this.is_show) this.is_show = true;
+    this.is_running = !this.is_running;
+    console.log(this.is_running)
+    if (this.is_running){
+      const intval = setInterval(() =>{
+        this.clock_progress += 30;
+      }, 1000)
+    }
+    else{
+      console.log("i am at else")
+    }
+
+    this.eventEmmiter.emit(!this.isClicked)
+
   }
 }
